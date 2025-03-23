c9e2d23e54150f9103a744b83d990753d59677cc
Manpreet123442 <manpreet123442@gmail.com>
2025-03-24 00:01:58 +0530

    Final commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/dashboard',
+  base: '/admin/',
   build: {
     outDir: 'dist',
   }
 });
+
+// AdminPanelByManpreet
